a7b4bfa626a2fb613bad812ca48bc08a25a7282e
sedateLuoX <crazyluox@qq.com>
2021-11-19 22:43:11 +0800

    spring 项目构建

--- spring-lx/src/main/java/com/lx/spring/Teach.java
+++ spring-lx/src/main/java/com/lx/spring/Teach.java
@@ -7,6 +7,7 @@ public class Teach {
 	public static void main(String[] args) {
 
 
+		//
 		AnnotationConfigApplicationContext  context = new AnnotationConfigApplicationContext(AppConfig.class);
 
 	}
