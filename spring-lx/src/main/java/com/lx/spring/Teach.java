6ee80eb6f217e68307c261c0a7ab9e0f53fb3e89
sedateLuoX <crazyluox@qq.com>
2021-11-19 22:54:50 +0800

    add 添加文件注释

--- spring-lx/src/main/java/com/lx/spring/Teach.java
+++ spring-lx/src/main/java/com/lx/spring/Teach.java
@@ -7,7 +7,7 @@ public class Teach {
 	public static void main(String[] args) {
 
 
-		//
+		// 添加文件注释
 		AnnotationConfigApplicationContext  context = new AnnotationConfigApplicationContext(AppConfig.class);
 
 	}
