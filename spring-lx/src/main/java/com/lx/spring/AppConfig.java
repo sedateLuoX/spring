12d2d060806b2ac7d18fc9b271a3b7713866c26d
sedateLuoX <crazyluox@qq.com>
2021-11-19 22:39:56 +0800

    spring 项目构建

---
+++ spring-lx/src/main/java/com/lx/spring/AppConfig.java
@@ -0,0 +1,8 @@
+package com.lx.spring;
+
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+}
---
+++ spring-lx/src/main/java/com/lx/spring/Teach.java
@@ -0,0 +1,13 @@
+package com.lx.spring;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Teach {
+
+	public static void main(String[] args) {
+
+
+		AnnotationConfigApplicationContext  context = new AnnotationConfigApplicationContext(AppConfig.class);
+
+	}
+}
